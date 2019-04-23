d52a0a386b473bed556b5fac43ace1cea38bdbba
soffiafdz <so1.618e@gmail.com>
2019-04-23 12:37:37 -0400

    Created directory for easy handling

--- dock2singXcpEng.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to convert Docker image into Singularity 
-# xcpEngine docker image
-#old version of singularity that exports images compatible with LAVIS
-
-docker run -v /var/run/docker.sock:/var/run/docker.sock \
-	-v /home/soffiafdz/Documents/lavisScripts:/output --privileged \
-	-t --rm singularityware/docker2singularity:v2.3 pennbbl/xcpengine
