3b9c8303d22917b25cf6e3f59e66f514a2dfbd08
soffiafdz <so1.618e@gmail.com>
2019-04-23 11:20:11 -0400

    Ignore created singularity image

--- dock2singXcpEng.sh
+++ dock2singXcpEng.sh
@@ -3,6 +3,6 @@
 # xcpEngine docker image
 #old version of singularity that exports images compatible with LAVIS
 
-docker run -v /var/run/docker.sock:/var/run/docker/sock \
+docker run -v /var/run/docker.sock:/var/run/docker.sock \
 	-v /home/soffiafdz/Documents/lavisScripts:/output --privileged \
 	-t --rm singularityware/docker2singularity:v2.3 pennbbl/xcpengine
